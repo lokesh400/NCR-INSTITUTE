5d4f4124e13ab84c7dc3b8d0c3701e6db3070799
thetestpulse <lokeshbadgujjar400@gmail.com>
2025-03-04 13:57:15 +0530

    commit

--- routes/job.js
+++ routes/job.js
@@ -40,32 +40,38 @@ router.delete('/jobs/:id', async (req, res) => {
 });
 
 // Route to apply for a job
+router.get('/apply/:id', async (req, res) => {
+    try {
+       const {id} = req.params;
+       res.render('admin/applyThisJob.ejs',{id})
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
 router.post('/:id/apply', async (req, res) => {
     try {
         const { name, mobile, email, qualification } = req.body;
         const job = await Job.findById(req.params.id);
-
         if (!job) return res.status(404).send("Job not found");
-
         job.applications.push({ name, mobile, email, qualification });
         await job.save();
-
         res.redirect(`/jobs/${req.params.id}`); // Redirect to the job details page
     } catch (error) {
         res.status(500).send("Server Error");
     }
 });
 
-// specific job applications
-router.get('/:id/applications', async (req, res) => {
+router.get('/jobs/:id/applications', async (req, res) => {
     try {
         const job = await Job.findById(req.params.id);
-
-        if (!job) return res.status(404).json({ message: "Job not found" });
-
-        res.json({ applications: job.applications }); // Return all applications
+        if (!job) {
+            return res.status(404).send("Job not found");
+        }
+        res.render('admin/viewThisJob.ejs', { job });
     } catch (error) {
-        res.status(500).json({ message: "Server error" });
+        console.error(error);
+        res.status(500).send("Internal Server Error");
     }
 });
 
